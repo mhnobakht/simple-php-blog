4056b43f9785c6c554263ae69cf61231ea71fe27
academy01 <80899303+mhnobakht@users.noreply.github.com>
2023-06-19 15:57:08 +0330

    Modify index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+require_once 'autoload.php';
+
+$post = new Post();
+$posts = $post->getAll();
+
+$test = $posts[0];
+
+// var_dump($test['user']);die;
+
+?>
 <!DOCTYPE html>
 <html lang="en">
     <?php 
@@ -39,59 +50,34 @@
         <div class="container px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
+                    <?php
+                    foreach($posts as $post):
+                    ?>
                     <!-- Post preview-->
                     <div class="post-preview">
                         <a href="post.html">
-                            <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
-                            <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
+                            <h2 class="post-title"><?php echo $post['title']; ?></h2>
+                            <h3 class="post-subtitle"><?php echo mb_strimwidth($post['description'], 0, 50, '...'); ?></h3>
                         </a>
                         <p class="post-meta">
                             Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on September 24, 2022
+                            <a href="#!">
+                                <?php
+                                echo $post['user']['firstname'].' '.$post['user']['lastname'];
+                                ?>
+                            </a>
+                            <!-- on September 24, 2022 -->
+                            on 
+                            <?php
+                            echo date("F j\, Y", strtotime($post['created_at']));
+                            ?>
                         </p>
                     </div>
                     <!-- Divider-->
                     <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html"><h2 class="post-title">I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.</h2></a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on September 18, 2022
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Science has not yet mastered prophecy</h2>
-                            <h3 class="post-subtitle">We predict too much for the next year and yet far too little for the next ten.</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on August 24, 2022
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Failure is not an option</h2>
-                            <h3 class="post-subtitle">Many say exploration is part of our destiny, but it’s actually our duty to future generations.</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on July 8, 2022
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
+                    <?php
+                    endforeach;
+                    ?>
                     <!-- Pager-->
                     <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
                 </div>
